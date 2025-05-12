9dc1feee179efdf86ccad6acb70f1c675ce4c6f6
Matt Davis <matt.davis06@gmail.com>
2025-05-12 18:29:17 +0100

    initial commit - app init setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { User } from 'lucide-react'
+
+export default function Home() {
+	return (
+		<div>
+			<h1 className='text-primary-dark-blue'>Hello World</h1>
+
+			<User />
+		</div>
+	)
+}
